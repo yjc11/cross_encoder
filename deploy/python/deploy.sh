cc84a8a06adfebb86afb47ee430e616a9326fa85
yjc11 <youmcgrady@126.com>
2023-05-16 03:08:12 +0000

    [bug fix]softmax维度错误

--- deploy/python/deploy.sh
+++ deploy/python/deploy.sh
@@ -1,3 +1,9 @@
 python deploy/python/predict.py --model_dir ./output \
-                                --input_file data/test.csv \
+                                --max_seq_length 512 \
+                                --batch_size 32 \
+                                --device gpu \
+                                --precision fp32 \
+                                --cpu_threads 10 \
+                                --save_log_path ./log_output/ \
+                                --input_file ./workspace/data/test.tsv \
                                 --model_name_or_path rocketqa-base-cross-encoder
--- deploy/python/predict.py
+++ deploy/python/predict.py
@@ -34,7 +34,7 @@ parser.add_argument("--model_dir", type=str, required=True, help="The directory 
 parser.add_argument("--input_file", type=str, required=True, help="The test set file.")
 parser.add_argument("--max_seq_length", default=512, type=int, help="The maximum total input sequence length after tokenization. Sequences longer than this will be truncated, sequences shorter will be padded.")
 parser.add_argument("--batch_size", default=32, type=int, help="Batch size per GPU/CPU for training.")
-parser.add_argument('--device', choices=['cpu', 'gpu', 'xpu'], default="gpu", help="Select which device to train model, defaults to gpu.")
+parser.add_argument('--device', default="gpu", help="Select which device to train model, defaults to gpu.")
 parser.add_argument('--use_tensorrt', default=False, type=eval, choices=[True, False], help='Enable to use tensorrt to speed up.')
 parser.add_argument("--precision", default="fp32", type=str, choices=["fp32", "fp16", "int8"], help='The tensorrt precision.')
 parser.add_argument('--cpu_threads', default=10, type=int, help='Number of threads to predict when using cpu.')
@@ -191,8 +191,7 @@ class Predictor(object):
         sim_score = self.output_handle.copy_to_cpu()
         if args.benchmark:
             self.autolog.times.stamp()
-        sim_score = softmax(sim_score)[:, 1]
-
+        sim_score = softmax(sim_score, axis=1)[:, 1]
         if args.benchmark:
             self.autolog.times.end(stamp=True)
 
